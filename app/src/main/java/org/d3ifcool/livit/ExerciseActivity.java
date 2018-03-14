e3257f974828e7aca2428409ed65bd6f64f40a98
ernarahmawatirizal@gmail.com <ernarahmawatirizal05>
2018-03-14 15:08:23 +0700

    toaste the nutrition buttons

--- app/src/main/java/org/d3ifcool/livit/ExerciseActivity.java
+++ app/src/main/java/org/d3ifcool/livit/ExerciseActivity.java
@@ -2,6 +2,8 @@ package org.d3ifcool.livit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ExerciseActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ExerciseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        View actionButtons = findViewById(R.id.action_buttons);
+        actionButtons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ExerciseActivity.this,"Features not implemented yet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
